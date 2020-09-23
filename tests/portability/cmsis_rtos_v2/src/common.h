f4f06478853c239646ec3d08d8909f752162ada8
Andrew Boie <andrew.p.boie@intel.com>
2020-09-23 10:34:39 -0700

    tests: cmsis: v2: add common header
    Better to do this here than in just main.c.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

---
+++ tests/portability/cmsis_rtos_v2/src/common.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef COMMON_H
+#define COMMON_H
+
+extern void test_kernel_apis(void);
+extern void test_delay(void);
+extern void test_thread_apis(void);
+extern void test_thread_apis_dynamic(void);
+extern void test_thread_prio(void);
+extern void test_thread_prio_dynamic(void);
+extern void test_timer(void);
+extern void test_mutex(void);
+extern void test_mutex_lock_timeout(void);
+extern void test_semaphore(void);
+extern void test_mempool(void);
+extern void test_mempool_dynamic(void);
+extern void test_messageq(void);
+extern void test_event_flags_no_wait_timeout(void);
+extern void test_event_flags_signalled(void);
+extern void test_event_flags_isr(void);
+extern void test_thread_flags_no_wait_timeout(void);
+extern void test_thread_flags_signalled(void);
+extern void test_thread_flags_isr(void);
+extern void test_thread_join(void);
+extern void test_thread_detached(void);
+extern void test_thread_joinable_detach(void);
+extern void test_thread_joinable_terminate(void);
+
+#endif
--- tests/portability/cmsis_rtos_v2/src/main.c
+++ tests/portability/cmsis_rtos_v2/src/main.c
@@ -7,30 +7,7 @@
 #include <ztest.h>
 #include <kernel.h>
 #include <cmsis_os2.h>
-
-extern void test_kernel_apis(void);
-extern void test_delay(void);
-extern void test_thread_apis(void);
-extern void test_thread_apis_dynamic(void);
-extern void test_thread_prio(void);
-extern void test_thread_prio_dynamic(void);
-extern void test_timer(void);
-extern void test_mutex(void);
-extern void test_mutex_lock_timeout(void);
-extern void test_semaphore(void);
-extern void test_mempool(void);
-extern void test_mempool_dynamic(void);
-extern void test_messageq(void);
-extern void test_event_flags_no_wait_timeout(void);
-extern void test_event_flags_signalled(void);
-extern void test_event_flags_isr(void);
-extern void test_thread_flags_no_wait_timeout(void);
-extern void test_thread_flags_signalled(void);
-extern void test_thread_flags_isr(void);
-extern void test_thread_join(void);
-extern void test_thread_detached(void);
-extern void test_thread_joinable_detach(void);
-extern void test_thread_joinable_terminate(void);
+#include "common.h"
 
 void test_main(void)
 {
