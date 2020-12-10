287ffc5798b4394e1a33911ffed6f0a5ebc840f0
Andrew Boie <andrew.p.boie@intel.com>
2020-12-10 12:56:12 -0800

    kernel: add demand paging internal interfaces
    APIs used by backing store and eviction algorithms.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- kernel/include/mmu.h
+++ kernel/include/mmu.h
@@ -60,6 +60,12 @@ struct z_page_frame {
 	 * needed.
 	 */
 	uint32_t flags;
+
+#ifdef CONFIG_DEMAND_PAGING
+	/* TODO: Additional data structures may go here specific to the page
+	 * eviction algorithm in use.
+	 */
+#endif
 };
 
 static inline bool z_page_frame_is_pinned(struct z_page_frame *pf)
@@ -171,4 +177,132 @@ static inline void z_mem_assert_virtual_region(void *addr, size_t size)
 
 #define Z_VM_RESERVED	0
 
+#ifdef CONFIG_DEMAND_PAGING
+/* We reserve a virtual page as a scratch area for page-ins/outs at the end
+ * of the address space
+ */
+#define Z_VM_RESERVED	CONFIG_MMU_PAGE_SIZE
+#define Z_SCRATCH_PAGE	((void *)((uintptr_t)CONFIG_KERNEL_VM_BASE + \
+				     (uintptr_t)CONFIG_KERNEL_VM_SIZE - \
+				     CONFIG_MMU_PAGE_SIZE))
+#else
+#define Z_VM_RESERVED	0
+#endif
+
+#ifdef CONFIG_DEMAND_PAGING
+/*
+ * Eviction algorihm APIs
+ */
+
+/**
+ * Select a page frame for eviction
+ *
+ * The kernel will invoke this to choose a page frame to evict if there
+ * are no free page frames.
+ *
+ * This function will never be called before the initial z_eviction_init().
+ *
+ * This function is invoked with interrupts locked.
+ *
+ * @param [out] Whether the page to evict is dirty
+ * @return The page frame to evict
+ */
+struct z_page_frame *z_eviction_select(bool *dirty);
+
+/**
+ * Initialization function
+ *
+ * Called at early boot to perform any necessary initialization tasks for the
+ * eviction algorithm. z_eviction_init() is guaranteed to never be called
+ * until this has returned, and this will only be called once.
+ */
+void z_eviction_init(void);
+
+/*
+ * Backing store APIs
+ */
+
+/**
+ * Reserve or fetch a storage location for a data page
+ *
+ * The returned location token must be:
+ * 1) Unique to the provided virtual address
+ * 2) Aligned to CONFIG_MMU_PAGE_SIZE
+ * 3) Consistent between calls, unless z_backing_store_location_free() has
+ *    been invoked, in which case any associated data at the storage location
+ *    will be discarded.
+ *
+ * This location will be used in the backing store to page out data page
+ * contents for later retrieval.
+ *
+ * @param addr Data page virtual address
+ * @return storage location token
+ */
+uintptr_t z_backing_store_location_get(void *addr);
+
+/**
+ * Free a backing store location for a virtual data page
+ *
+ * Any stored data may be discarded, and the location token associated with
+ * this address may be re-used for some other data page.
+ *
+ * @param addr Data page virtual address
+ */
+void z_backing_store_location_free(void *addr);
+
+/**
+ * Copy a data page from Z_SCRATCH_PAGE to the specified location
+ *
+ * Immediately before this is called, Z_SCRATCH_PAGE will be mapped read-write
+ * to the intended source page frame for the calling context.
+ *
+ * Calls to this and z_backing_store_page_in() will always be serialized,
+ * but interrupts may be enabled.
+ *
+ * @param location Location token for the data page, for later retrieval
+ */
+void z_backing_store_page_out(uintptr_t location);
+
+/**
+ * Copy a data page from the provided location to Z_SCRATCH_PAGE.
+ *
+ * Immediately before this is called, Z_SCRATCH_PAGE will be mapped read-write
+ * to the intended destination page frame for the calling context.
+ *
+ * Calls to this and z_backing_store_page_out() will always be serialized,
+ * but interrupts may be enabled.
+ *
+ * @param location Location token for the data page
+ */
+void z_backing_store_page_in(uintptr_t location);
+
+/**
+ * Backing store initialization function.
+ *
+ * The implementation may expect to receive page in/out calls as soon as this
+ * returns, but not before that. Called at early kernel initialization.
+ */
+void z_backing_store_init(void);
+
+/*
+ * Core kernel demand paging APIs
+ */
+
+/**
+ * Free a page frame physical address by evicting its contents
+ *
+ * The indicated page frame, if it contains a data page, will have that
+ * data page evicted to the backing store. The page frame will then be
+ * marked as available for mappings or page-ins.
+ *
+ * This is useful for freeing up entire memory banks so that they may be
+ * deactivated to save power.
+ *
+ * If CONFIG_DEMAND_PAGING_ALLOW_IRQ is enabled, this function may not be
+ * called by ISRs as the backing store may be in-use.
+ *
+ * @param phys Page frame physical address
+ */
+void z_page_frame_evict(uintptr_t phys);
+#endif /* CONFIG_DEMAND_PAGING */
 #endif /* KERNEL_INCLUDE_MMU_H */
